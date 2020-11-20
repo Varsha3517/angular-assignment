e7b844b8c55ea40fc2f7ad88f469927b72fb0c49
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-21 02:32:16 +0530

    Added few styles in login page and added change-password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatFormFieldModule} from '@angular/material/form-field'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,19 +9,35 @@ import { LoginComponent } from './components/login/login.component';
 
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { CheckEmailComponent } from './components/check-email/check-email.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+  
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeScreenComponent,
-    CheckEmailComponent
+    CheckEmailComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const modules = [
+  MatFormFieldModule,
+];
+
+@NgModule({
+imports: [...modules],
+exports: [...modules]
+,
+})export class MaterialModule {};
