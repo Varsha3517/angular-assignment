57a101097e944788310dfaffec948512a6385fbe
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-24 00:25:52 +0530

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChangePasswordComponent} from './components/change-password/change-password.component';
+import { CheckEmailComponent} from './components/check-email/check-email.component';
+import { LoginComponent} from './components/login/login.component';
+import { HomeScreenComponent} from './components/home-screen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'check-email', component: CheckEmailComponent},
+  { path: 'home-screen', component: HomeScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
