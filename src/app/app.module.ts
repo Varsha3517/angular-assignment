a782fd4fccce39f3015c66dfc00fe5ce0ec91f34
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-24 20:39:21 +0530

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MatFormFieldModule} from '@angular/material/form-field'
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,10 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { CheckEmailComponent } from './components/check-email/check-email.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
+import { SampleComponent } from './components/sample/sample.component';
+import { FormsModule } from '@angular/forms';
   
 
 @NgModule({
@@ -18,11 +24,17 @@ import { ChangePasswordComponent } from './components/change-password/change-pas
     LoginComponent,
     HomeScreenComponent,
     CheckEmailComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    SampleComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, 
+    MatDatepickerModule,
+    MatInputModule
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
@@ -41,3 +53,41 @@ imports: [...modules],
 exports: [...modules]
 ,
 })export class MaterialModule {};
+
+
+
+
+
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+// import { MaterialFileInputModule } from 'ngx-material-file-input';
+// import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule
+  //  MaterialFileInputModule,
+  //  MatIconModule,
+
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule
+  //  MaterialFileInputModule,
+  //  MatIconModule,
+
+  ]
+})
+
+export class AngularMaterialsModule { }
