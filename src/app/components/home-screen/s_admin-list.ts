e6ad2d9444a7c184ce1c6b15a84b15628acf4544
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-24 20:42:18 +0530

    Added s_admin.ts file

---
+++ src/app/components/home-screen/s_admin-list.ts
@@ -0,0 +1,14 @@
+import { Superadmin } from './s_admin';
+
+export const SUPERADMIN: Superadmin[] = [
+    {
+        s_name: "Kevin",
+        s_employeecode: "RT12345",
+        s_mail: "kevin@robosoftin.com"
+    },
+    {
+        s_name: "Sowmya",
+        s_employeecode: "RT23456",
+        s_mail: "sowmya@robosoftin.com"
+    }
+];
