e8bc98fcafe9a3ec5c90e37308c2984d189d0fb3
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-24 20:43:20 +0530

    Updated

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
-
-
-/** @title Basic datepicker */
-/*
-@Component({
-  selector: 'datepicker-overview-example',
-  templateUrl: 'datepicker-overview-example.html',
-})
-export class DatepickerOverviewExample {}
-*/
+import { AdminService } from 'src/app/service/admin.service';
+import { SuperadminService} from 'src/app/service/superadmin.service';
+import { Admin } from './admin';
+import { Superadmin } from './s_admin';
 
 @Component({
   selector: 'app-home-screen',
@@ -18,21 +12,45 @@ export class DatepickerOverviewExample {}
   styleUrls: ['./home-screen.component.scss']
 })
 
-
 export class HomeScreenComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  admin: Admin = {
+    name: "",
+    employeeCode: "",
+    mail: ""
   }
 
-  options():void {
+  adminlist: Admin[];
+
+  s_admin: Superadmin = {
+    s_name: "",
+    s_employeecode: "",
+    s_mail: ""
+  }
+
+  superadminlist: Superadmin[];
+
+  constructor(private adminservice: AdminService, private superadminservice: SuperadminService) { }
+
+  ngOnInit(): void {
+    this.loadAdmins();
+    this.loadSuperAdmins();
+  }
+
+  loadAdmins(): void {
+    this.adminlist = this.adminservice.getAdmins();
+  }
+
+  loadSuperAdmins(): void {
+    this.superadminlist = this.superadminservice.getSuperAdmins();
+  }
+
+  options(): void {
     document.getElementById('container').style.zIndex = "1";
     document.getElementById('options').style.zIndex = "5";
   }
 
-  disappear(): void
-  {
+  disappear(): void {
     document.getElementById('container').style.zIndex = "5";
     document.getElementById('screen').style.zIndex = "3";
     document.getElementById('options').style.zIndex = "1";
@@ -40,8 +58,7 @@ export class HomeScreenComponent implements OnInit {
     document.getElementById('admin').style.zIndex = "-5";
   }
 
-  close(): void
-  {
+  close(): void {
     document.getElementById('container').style.zIndex = "5";
     document.getElementById('screen').style.zIndex = "3";
     document.getElementById('options').style.zIndex = "1";
@@ -49,8 +66,7 @@ export class HomeScreenComponent implements OnInit {
     document.getElementById('admin').style.zIndex = "-5";
   }
 
-  update(): void
-  {
+  update(): void {
     document.getElementById('container').style.zIndex = "5";
     document.getElementById('screen').style.zIndex = "3";
     document.getElementById('options').style.zIndex = "1";
@@ -58,8 +74,7 @@ export class HomeScreenComponent implements OnInit {
     document.getElementById('admin').style.zIndex = "-5";
   }
 
-  superAdmin(): void
-  {
+  superAdmins(): void {
     document.getElementById('container').style.zIndex = "1";
     document.getElementById('screen').style.zIndex = "3";
     document.getElementById('options').style.zIndex = "-5";
@@ -67,8 +82,7 @@ export class HomeScreenComponent implements OnInit {
     document.getElementById('admin').style.zIndex = "-5";
   }
 
-  admin(): void
-  {
+  admins(): void {
     document.getElementById('container').style.zIndex = "1";
     document.getElementById('screen').style.zIndex = "3";
     document.getElementById('options').style.zIndex = "-5";
@@ -76,9 +90,33 @@ export class HomeScreenComponent implements OnInit {
     document.getElementById('admin').style.zIndex = "5";
   }
 
-  myFunction():void {
+  myFunction(): void {
     document.getElementById('container').style.zIndex = "-5";
   }
 
+  save1() {
+    const tempAdmin: Admin = { ...this.admin};
+    this.adminlist.unshift(tempAdmin)
+  }
 
+  save2() {
+    const tempSuperadmin: Superadmin = { ...this.s_admin};
+    this.superadminlist.unshift(tempSuperadmin)
+  }
+
+  delete1(admin: Admin): void {
+    this.adminlist.forEach((item, index) => {
+      if (item === admin) {
+        this.adminlist.splice(index, 1);
+      }
+    });
+  }
+
+  delete2(s_admin: Superadmin): void {
+    this.superadminlist.forEach((item, index) => {
+      if (item === s_admin) {
+        this.superadminlist.splice(index, 1);
+      }
+    });
+  }
 }
