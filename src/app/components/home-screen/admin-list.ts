d8576926e0ef8fa88cd7050c9480ecf5c534baff
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-24 20:40:57 +0530

    Added admin-list.ts file

---
+++ src/app/components/home-screen/admin-list.ts
@@ -0,0 +1,14 @@
+import { Admin } from './admin'
+
+export const ADMIN: Admin[] = [
+    {
+        name: "Rob",
+        employeeCode: "RT98765",
+        mail: "rob@robosoftin.com"
+    },
+    {
+        name: "Jane",
+        employeeCode: "RT54321",
+        mail: "jane@robosoft.com"
+    }
+];
