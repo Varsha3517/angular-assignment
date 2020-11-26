14ab0a015992e3093bdc85527287f6fcb30a5195
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-26 13:09:43 +0530

    Updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm : FormGroup = new FormGroup({
+    username : new FormControl(''),
+    password : new FormControl(''),
+  })
+
+  constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  authenticate() : void {
+    if(this.authService.login(this.userForm)) {
+      this.router.navigate(['home-screen']);
+    }
+    else {
+      alert("Login failed: Invalid Username or Password!");
+    }
+  }
 }
+
