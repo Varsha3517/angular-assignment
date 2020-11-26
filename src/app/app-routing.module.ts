5957c4a58f9b465ecaa1827454985aa902f5f9de
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-26 13:10:47 +0530

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ChangePasswordComponent} from './components/change-password/change-password.component';
-import { CheckEmailComponent} from './components/check-email/check-email.component';
-import { LoginComponent} from './components/login/login.component';
-import { HomeScreenComponent} from './components/home-screen/home-screen.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { CheckEmailComponent } from './components/check-email/check-email.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'login', component: LoginComponent},
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'change-password', component: ChangePasswordComponent },
-  { path: 'check-email', component: CheckEmailComponent},
-  { path: 'home-screen', component: HomeScreenComponent}
+  { path: 'check-email', component: CheckEmailComponent },
+  { path: 'home-screen', component: HomeScreenComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
