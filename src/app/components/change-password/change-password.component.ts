85b4e168455ba21a67ff327525bc7e403d13c85a
Varsha3517 <72905907+Varsha3517@users.noreply.github.com>
2020-11-26 13:10:06 +0530

    Updated

--- src/app/components/change-password/change-password.component.ts
+++ src/app/components/change-password/change-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: AuthService, private router: Router) { }
+  changepassword: FormGroup = new FormGroup({
+    newPassword: new FormControl('', [Validators.required]),
+    reEnterPassword: new FormControl('', [Validators.required])
+  });
 
   ngOnInit(): void {
   }
 
+  verifyPassword() {
+    if (this.changepassword.value.newPassword === this.changepassword.value.reEnterPassword) {
+      alert("Password successfully changed!");
+      this.router.navigate(['login']);
+    }
+    else {
+      alert("Passwords do not match!");
+      this.router.navigate(['change-password']);
+    }
+  }
 }
